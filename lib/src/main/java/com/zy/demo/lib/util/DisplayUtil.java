089d79f954b89a5126d1fb9998a71121e90e646f
张璐琳 <413700858@qq.com>
2016-07-31 18:36:49 +0800

    提交 DisplayUtil

---
+++ lib/src/main/java/com/zy/demo/lib/util/DisplayUtil.java
@@ -0,0 +1,66 @@
+package com.zy.demo.lib.util;
+
+
+import android.content.Context;
+import android.graphics.Paint;
+
+/**
+ * Create by zhangll on 2016.07.31
+ */
+public class DisplayUtil {
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     *
+     * @param pxValue
+     *            （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     *
+     * @param dipValue
+     *            （DisplayMetrics类中属性density）
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue
+     *            （DisplayMetrics类中属性scaledDensity）
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * @return 返回指定笔的文字高度偏移量
+     */
+    public static float getFontHeightOff(Paint paint)  {
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        return (fontMetrics.descent- fontMetrics.ascent)/2 - fontMetrics.descent;
+    }
+}
