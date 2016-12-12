c7cb5edb0cbc720fce50a8093cad97b18af1ec27
张璐琳 <413700858@qq.com>
2016-12-12 10:58:14 +0800

    新增对『不保留活动模式』的测试 demo

---
+++ demo/src/main/java/com/zy/open/demo/bundle/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.zy.open.demo.bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zy.open.demo.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
--- demo/src/main/java/com/zy/open/demo/bundle/ToActivity.java
+++ demo/src/main/java/com/zy/open/demo/bundle/ToActivity.java
@@ -1,8 +1,11 @@
 package com.zy.open.demo.bundle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.zy.open.lib.base.ZYActivity;
 import com.zy.open.demo.R;
@@ -14,22 +17,40 @@ import com.zy.open.demo.R;
  */
 public class ToActivity extends ZYActivity {
 
+    int data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("LifeCircle", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to);
+
+        findViewById(R.id.btn_send_bundle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ToActivity.this, ThirdActivity.class);
+                ToActivity.this.startActivity(intent);
+            }
+        });
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("data = " + data);
     }
 
     @Override
     protected void initData(Bundle savedBundle) {
         super.initData(savedBundle);
-        int data = savedBundle.getInt("data");
+        data = savedBundle.getInt("data");
         Log.i("bundle", " data =  " + data);
 
         //提供用来保存额外参数的 api
         saveToBundle("intData", data + 23);
         Log.i("bundle", " data =  " + savedBundle.getInt("intData"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
